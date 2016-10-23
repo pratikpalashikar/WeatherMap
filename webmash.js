5b09227d0009ed3f78d1e4963ffc7160e2bcdd54
algorithmFreak <pratikpalashikar@gmail.com>
2016-10-23 02:55:23 -0500

    weather history with markers

--- webmash.js
+++ webmash.js
@@ -5,93 +5,129 @@ var request = new XMLHttpRequest();
 var map;
 var markerArray = [];
 var contentString = "";
+var addressList = new Array();
 function initMap() {
 
-	var myLatLng = {lat: 32.75, lng: -97.13};
-	var marker;
-	map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: 32.75, lng: -97.13},
-			zoom: 17
-	});
-	var infowindow = new google.maps.InfoWindow({
-          content: contentString
-  });
+    var myLatLng = {
+        lat: 32.75,
+        lng: -97.13
+    };
+    var geocoder = new google.maps.Geocoder;
+    var marker;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 32.75,
+            lng: -97.13
+        },
+        zoom: 17
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
-	function placeMarker(myLatLng){
-	 clearMarkers();
-	 marker = new google.maps.Marker({
-					 position: myLatLng,
-					 map: map
-	 });
-	google.maps.event.addListener(marker,'mouseover', function(event) {
- 			infowindow.open(map,marker);
- 		});
-	 markerArray.push(marker);
-	 }
-	google.maps.event.addListener(map,'click', function(event) {
-		contentString = "";
-		placeMarker(event.latLng);
-		reversegeocode(event.latLng,infowindow);
-		infowindow.open(map,marker);
-	});
+    function placeMarker(myLatLng) {
+        clearMarkers();
+        marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'mouseover', function(event) {
+            infowindow.open(map, marker);
+        });
+        markerArray.push(marker);
+
+    }
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        contentString = "";
+        placeMarker(event.latLng);
+        reversegeocode(event.latLng, infowindow, geocoder);
+        infowindow.open(map, marker);
+    });
+
 }
 
 
+function clean(){
+	addressList = [];
+	  document.getElementById("result").innerHTML = "";
 
+}
 
 function setMapOnAll(map) {
-			 for (var i = 0; i < markerArray.length; i++) {
-				 markerArray[i].setMap(map);
-				}
+    for (var i = 0; i < markerArray.length; i++) {
+        markerArray[i].setMap(map);
+    }
 }
 
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers(markerArray) {
-			 setMapOnAll(null);
+    setMapOnAll(null);
 }
 
 //Wait for some time before proceeding
-function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
 }
 
 // Reserse Geocoding
-function reversegeocode(myLatLng,infowindow) {
+function reversegeocode(myLatLng, infowindow, geocoder) {
 
-  //get the latitude and longitude from the mouse click and get the address
-	var lat = myLatLng.lat();
-	var lng = myLatLng.lng();
-	sendRequest(lat,lng,infowindow);
+    //get the latitude and longitude from the mouse click and get the address
+    var lat = myLatLng.lat();
+    var lng = myLatLng.lng();
+    sendRequest(lat, lng, infowindow, geocoder);
 
-}// end of geocodeLatLng()
+} // end of geocodeLatLng()
 
 
 
-function displayResult () {
+function displayResult() {
     if (request.readyState == 4) {
-			//Not used
+        //Not used
     }
 }
-*/
-function sendRequest (lat,lng,infowindow) {
-  //  request.onreadystatechange = displayResult;
-    request.open("GET","http://api.geonames.org/findNearByWeatherXML?lat="+lat+"&lng="+lng+"&username="+username);
-		request.onreadystatechange = function() {
-		    if (request.readyState == 4) {
 
-		        var xml = request.responseXML.documentElement;
-						var temperature = xml.getElementsByTagName("temperature")[0].childNodes[0].nodeValue;
-						var clouds = xml.getElementsByTagName("clouds")[0].childNodes[0].nodeValue;
-						var windspeed =  xml.getElementsByTagName("windSpeed")[0].childNodes[0].nodeValue;
-						contentString += "Temp : "+ temperature +" Clouds :"+clouds+" WindSpeed :"+windspeed;
-						console.log("inside dispresult" + contentString);
-						infowindow.setContent(contentString);
+function sendRequest(lat, lng, infowindow, geocoder) {
+    //  request.onreadystatechange = displayResult;
+    request.open("GET", "http://api.geonames.org/findNearByWeatherXML?lat=" + lat + "&lng=" + lng + "&username=" + username);
+    request.onreadystatechange = function() {
+        var address;
 
-		    }
-		};
-		  request.send(null);
+        if (request.readyState == 4) {
+
+
+            var xml = request.responseXML.documentElement;
+            var temperature = xml.getElementsByTagName("temperature")[0].childNodes[0].nodeValue;
+            var clouds = xml.getElementsByTagName("clouds")[0].childNodes[0].nodeValue;
+            var windspeed = xml.getElementsByTagName("windSpeed")[0].childNodes[0].nodeValue;
+            contentString += "Temp : " + temperature + " Clouds :" + clouds + " WindSpeed :" + windspeed;
+            //Get the address as well by doing the reverse geocode
+            var latLng = {
+                lat: lat,
+                lng: lng
+            };
+            geocoder.geocode({
+                'location': latLng
+            }, function(results, status) {
+                if (status == "OK") {
+                    address = results[1].formatted_address;
+                    infowindow.setContent(contentString + "<br>" + address);
+                    addressList.push(address);
+										document.getElementById("result").innerHTML += "<li>" + address + "</li>";
+                  /*  for (var i = 0; i < addressList.length; i++) {
+                        document.getElementById("result").innerHTML += "<li>" + addressList[i] + "</li>";
+                    }*/
+                }
+            });
+
+
+        }
+    };
+    request.send(null);
 }
